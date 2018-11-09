4cabf303d0070ac8f516985c0e5641099f690023
Enrique <qui.per.gonz32@gmail.com>
2018-11-09 19:30:22 +0100

    Updates

---
+++ precache-manifest.1c551f270c6790d331b4b2caf80500f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e924ea290600ad834bb7",
+    "url": "/ListCharactersMarvel/static/js/runtime~main.e924ea29.js"
+  },
+  {
+    "revision": "f24c3ec7aeaefaf74801",
+    "url": "/ListCharactersMarvel/static/js/main.f24c3ec7.chunk.js"
+  },
+  {
+    "revision": "ed528641ba9f30933048",
+    "url": "/ListCharactersMarvel/static/js/1.ed528641.chunk.js"
+  },
+  {
+    "revision": "f24c3ec7aeaefaf74801",
+    "url": "/ListCharactersMarvel/static/css/main.a79d85ce.chunk.css"
+  },
+  {
+    "revision": "efaf7e7cfaed40da4bd522b2092f706b",
+    "url": "/ListCharactersMarvel/index.html"
+  }
+];
